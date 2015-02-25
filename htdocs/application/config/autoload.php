9556d74027ab88cff76dc811f6f64d79bf2f8ba8
Arlette-Liliane <aemebiku@student.42.fr>
2015-02-25 20:31:53 +0100

    modify

--- htdocs/application/config/autoload.php
+++ htdocs/application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('Aauth', 'form_validation', 'database');
+$autoload['libraries'] = array('Aauth', 'form_validation', 'database', 'Phpmailer');
 
 
 /*
