148ec5ea540010c7fec78ceaf6194792332e41cd
Arlette-Liliane <aemebiku@student.42.fr>
2015-06-03 19:59:30 +0200

    nouvel instal de mamp et modif

--- 42framework.sql
+++ 42framework.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.9
+-- version 4.4.7
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:3306
--- Généré le :  Ven 22 Mai 2015 à 12:27
--- Version du serveur :  5.5.36
--- Version de PHP :  5.4.26
+-- Généré le :  Mer 03 Juin 2015 à 10:58
+-- Version du serveur :  5.6.20
+-- Version de PHP :  5.5.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,11 +27,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `aauth_groups` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` text,
-  PRIMARY KEY (`id`),
-  KEY `id_index` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+  `id` int(11) NOT NULL,
+  `name` text
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `aauth_groups`
@@ -49,12 +47,10 @@ INSERT INTO `aauth_groups` (`id`, `name`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `aauth_perms` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` text,
-  `definition` text,
-  PRIMARY KEY (`id`),
-  KEY `id_index` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `definition` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -63,12 +59,10 @@ CREATE TABLE IF NOT EXISTS `aauth_perms` (
 --
 
 CREATE TABLE IF NOT EXISTS `aauth_perm_to_group` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `perm_id` int(11) DEFAULT NULL,
-  `group_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `perm_id_group_id_index` (`perm_id`,`group_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `group_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -77,12 +71,10 @@ CREATE TABLE IF NOT EXISTS `aauth_perm_to_group` (
 --
 
 CREATE TABLE IF NOT EXISTS `aauth_perm_to_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `perm_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `perm_id_user_id_index` (`perm_id`,`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -91,16 +83,14 @@ CREATE TABLE IF NOT EXISTS `aauth_perm_to_user` (
 --
 
 CREATE TABLE IF NOT EXISTS `aauth_pms` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `sender_id` int(11) NOT NULL,
   `receiver_id` int(11) NOT NULL,
   `title` text NOT NULL,
   `message` text,
   `date` datetime DEFAULT NULL,
-  `read` int(11) DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `full_index` (`id`,`sender_id`,`receiver_id`,`read`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `read` int(11) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -109,11 +99,10 @@ CREATE TABLE IF NOT EXISTS `aauth_pms` (
 --
 
 CREATE TABLE IF NOT EXISTS `aauth_system_variables` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `key` text NOT NULL,
-  `value` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `value` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -122,7 +111,7 @@ CREATE TABLE IF NOT EXISTS `aauth_system_variables` (
 --
 
 CREATE TABLE IF NOT EXISTS `aauth_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `email` text NOT NULL,
   `pass` text NOT NULL,
   `name` text,
@@ -136,19 +125,18 @@ CREATE TABLE IF NOT EXISTS `aauth_users` (
   `remember_exp` text,
   `verification_code` text,
   `ip_address` text,
-  `login_attempts` int(11) DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `id_index` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+  `login_attempts` int(11) DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `aauth_users`
 --
 
 INSERT INTO `aauth_users` (`id`, `email`, `pass`, `name`, `surname`, `banned`, `last_login`, `last_activity`, `last_login_attempt`, `forgot_exp`, `remember_time`, `remember_exp`, `verification_code`, `ip_address`, `login_attempts`) VALUES
-(6, 'admin@admin.com', '01f80f2a2962a97d02d9acf3dd9c8599b6386ab73702cb2f0f6f94fa7f114435', 'admin', 'admin', 0, '2015-05-13 10:22:30', '2015-05-13 10:22:30', '2015-05-13 10:00:00', NULL, NULL, NULL, NULL, '127.0.0.1', 1),
+(6, 'admin@admin.com', '01f80f2a2962a97d02d9acf3dd9c8599b6386ab73702cb2f0f6f94fa7f114435', 'admin', 'admin', 0, '2015-06-03 09:14:54', '2015-06-03 09:14:54', '2015-06-03 09:00:00', NULL, NULL, NULL, NULL, '127.0.0.1', 1),
 (7, 'tatiana@live.fr', 'f87f77fbef8e93a00a6124a605884b7fc4fa3d781485f344ff9a3e33d4bb03b7', 'taty', 'taty', 0, '2015-03-04 08:58:22', '2015-03-04 08:58:22', '2015-03-19 09:00:00', NULL, NULL, NULL, NULL, '127.0.0.1', 1),
-(8, 'agnes@live.com', '410a30ed8143b5dc4ff39bc9af6a665b4220295bd6732336728440310b65ec9e', 'Diane', 'Diane', 0, '2015-03-06 04:45:49', '2015-03-06 04:45:49', '2015-03-19 07:00:00', NULL, NULL, NULL, NULL, '127.0.0.1', 1);
+(8, 'agnes@live.com', '410a30ed8143b5dc4ff39bc9af6a665b4220295bd6732336728440310b65ec9e', 'Diane', 'Diane', 0, '2015-03-06 04:45:49', '2015-03-06 04:45:49', '2015-03-19 07:00:00', NULL, NULL, NULL, NULL, '127.0.0.1', 1),
+(9, 'ali@live.fr', '2f5a11d1f1ebc410f6d38f1aeba38fafb5efb726a487aa827c706cafa4d270a5', 'aline', 'aline', 0, '2015-06-03 10:31:37', '2015-06-03 10:31:37', '2015-06-03 10:00:00', NULL, NULL, NULL, NULL, '127.0.0.1', 1);
 
 -- --------------------------------------------------------
 
@@ -158,9 +146,7 @@ INSERT INTO `aauth_users` (`id`, `email`, `pass`, `name`, `surname`, `banned`, `
 
 CREATE TABLE IF NOT EXISTS `aauth_user_to_group` (
   `user_id` int(11) NOT NULL DEFAULT '0',
-  `group_id` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`user_id`,`group_id`),
-  KEY `user_id_group_id_index` (`user_id`,`group_id`)
+  `group_id` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -172,7 +158,8 @@ INSERT INTO `aauth_user_to_group` (`user_id`, `group_id`) VALUES
 (6, 3),
 (7, 2),
 (7, 3),
-(8, 3);
+(8, 3),
+(9, 3);
 
 -- --------------------------------------------------------
 
@@ -181,13 +168,11 @@ INSERT INTO `aauth_user_to_group` (`user_id`, `group_id`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `aauth_user_variables` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `key` text NOT NULL,
-  `value` text,
-  PRIMARY KEY (`id`),
-  KEY `user_id_index` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `value` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -196,12 +181,10 @@ CREATE TABLE IF NOT EXISTS `aauth_user_variables` (
 --
 
 CREATE TABLE IF NOT EXISTS `categories` (
-  `cat_id` int(8) NOT NULL AUTO_INCREMENT,
+  `cat_id` int(8) NOT NULL,
   `cat_name` varchar(255) NOT NULL,
-  `cat_description` varchar(255) NOT NULL,
-  PRIMARY KEY (`cat_id`),
-  UNIQUE KEY `cat_name_unique` (`cat_name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `cat_description` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `categories`
@@ -217,15 +200,12 @@ INSERT INTO `categories` (`cat_id`, `cat_name`, `cat_description`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `posts` (
-  `post_id` int(8) NOT NULL AUTO_INCREMENT,
+  `post_id` int(8) NOT NULL,
   `post_content` text NOT NULL,
   `post_date` datetime NOT NULL,
   `post_topic` int(8) NOT NULL,
-  `post_by` int(8) NOT NULL,
-  PRIMARY KEY (`post_id`),
-  KEY `post_topic` (`post_topic`),
-  KEY `post_by` (`post_by`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `post_by` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -234,16 +214,168 @@ CREATE TABLE IF NOT EXISTS `posts` (
 --
 
 CREATE TABLE IF NOT EXISTS `topics` (
-  `topic_id` int(8) NOT NULL AUTO_INCREMENT,
+  `topic_id` int(8) NOT NULL,
   `topic_subject` varchar(255) NOT NULL,
   `topic_date` datetime NOT NULL,
   `topic_cat` int(8) NOT NULL,
-  `topic_by` int(8) NOT NULL,
-  PRIMARY KEY (`topic_id`),
-  KEY `topic_cat` (`topic_cat`),
-  KEY `topic_by` (`topic_by`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `topic_by` int(8) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+--
+-- Contenu de la table `topics`
+--
+
+INSERT INTO `topics` (`topic_id`, `topic_subject`, `topic_date`, `topic_cat`, `topic_by`) VALUES
+(1, 'je suis aline', '2015-06-03 10:44:39', 1, 9);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `aauth_groups`
+--
+ALTER TABLE `aauth_groups`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_index` (`id`);
+
+--
+-- Index pour la table `aauth_perms`
+--
+ALTER TABLE `aauth_perms`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_index` (`id`);
+
+--
+-- Index pour la table `aauth_perm_to_group`
+--
+ALTER TABLE `aauth_perm_to_group`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `perm_id_group_id_index` (`perm_id`,`group_id`);
+
+--
+-- Index pour la table `aauth_perm_to_user`
+--
+ALTER TABLE `aauth_perm_to_user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `perm_id_user_id_index` (`perm_id`,`user_id`);
+
+--
+-- Index pour la table `aauth_pms`
+--
+ALTER TABLE `aauth_pms`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `full_index` (`id`,`sender_id`,`receiver_id`,`read`);
+
+--
+-- Index pour la table `aauth_system_variables`
+--
+ALTER TABLE `aauth_system_variables`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `aauth_users`
+--
+ALTER TABLE `aauth_users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_index` (`id`);
+
+--
+-- Index pour la table `aauth_user_to_group`
+--
+ALTER TABLE `aauth_user_to_group`
+  ADD PRIMARY KEY (`user_id`,`group_id`),
+  ADD KEY `user_id_group_id_index` (`user_id`,`group_id`);
+
+--
+-- Index pour la table `aauth_user_variables`
+--
+ALTER TABLE `aauth_user_variables`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id_index` (`user_id`);
+
+--
+-- Index pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`cat_id`),
+  ADD UNIQUE KEY `cat_name_unique` (`cat_name`);
+
+--
+-- Index pour la table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`post_id`),
+  ADD KEY `post_topic` (`post_topic`),
+  ADD KEY `post_by` (`post_by`);
+
+--
+-- Index pour la table `topics`
+--
+ALTER TABLE `topics`
+  ADD PRIMARY KEY (`topic_id`),
+  ADD KEY `topic_cat` (`topic_cat`),
+  ADD KEY `topic_by` (`topic_by`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `aauth_groups`
+--
+ALTER TABLE `aauth_groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `aauth_perms`
+--
+ALTER TABLE `aauth_perms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `aauth_perm_to_group`
+--
+ALTER TABLE `aauth_perm_to_group`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `aauth_perm_to_user`
+--
+ALTER TABLE `aauth_perm_to_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `aauth_pms`
+--
+ALTER TABLE `aauth_pms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `aauth_system_variables`
+--
+ALTER TABLE `aauth_system_variables`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `aauth_users`
+--
+ALTER TABLE `aauth_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT pour la table `aauth_user_variables`
+--
+ALTER TABLE `aauth_user_variables`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `cat_id` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `post_id` int(8) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `topics`
+--
+ALTER TABLE `topics`
+  MODIFY `topic_id` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- Contraintes pour les tables exportées
 --
